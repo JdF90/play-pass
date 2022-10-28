eca7bd71430b4c0f7bd9158ae2b60eecaa4f4e3c
Jelle de Fockert <j.de.fockert@topdesk.com>
2022-10-28 21:24:29 +0200

    Initial commit with the start button

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<start-page msg="Vite + Vue"/>
+</template>
+
+<script setup lang="ts">
+import StartPage from './components/StartPage.vue'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/StartPage.vue
@@ -0,0 +1,11 @@
+<template>
+	<button name="start-game-button">Start Game</button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
