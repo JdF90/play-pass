74669fbd3e67b3144a36ec44d233e759dccb01d3
Jelle de Fockert <j.de.fockert@topdesk.com>
2022-10-29 07:59:44 +0200

    Run linter and use the vue-eslint-parser for parsing the vue files.

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,12 @@ import StartPage from '../views/StartPage.vue';
 import {createRouter, createWebHistory} from 'vue-router';
 
 const routes = [
-    { path: '/', component: StartPage }
+	{ path: '/', component: StartPage }
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+	history: createWebHistory(),
+	routes,
 });
 
 export default router;
