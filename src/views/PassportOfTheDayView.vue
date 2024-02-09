b4f4680d610f0206d06f62edf3dbc0a150beb4ef
JdF90 <jwmdefockert@gmail.com>
2024-02-09 10:30:18 +0100

    Put max height on passport area

--- src/views/PassportOfTheDayView.vue
+++ src/views/PassportOfTheDayView.vue
@@ -1,16 +1,14 @@
 <template>
-    <main v-if="passport" class="flex flex-col gap-4 text-center">
-        <h1 class="mt-20 mb-8 text-white text-4xl">Passport of the day</h1>
+    <main v-if="passport" class="flex flex-col gap-4 text-center h-screen text-white ">
+        <h1 class="mt-20 mb-8text-4xl">Passport of the day</h1>
         <div v-if="!submitted">
-            <div>
-                <PassportVue :records="passport.records"/>
-            </div>
+            <PassportVue :records="passport.records" class="max-h-[500px] overflow-scroll"/>
             <div class="bottom-0 fixed flex justify-center gap-4 mt-8 mb-8 w-full">
                 <input v-model="answer" type="text" name="answer" id="answer" class="block w-1/4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-100 sm:text-sm sm:leading-6" placeholder="Enter your answer">
                 <button @click="submit" type="button" class="rounded-md bg-green-400 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500">Submit</button>
             </div>
         </div>
-        <div v-else class="flex flex-col place-items-center gap-5">
+        <div v-else class="flex flex-col gap-5 place-items-center">
             <p> {{ answerResponse?.isCorrect ? 'Correct!' : 'Wrong...' }} </p>
             <div class="h-10 w-[500px] inline-flex place-items-center">
                 <p> {{ answerResponse?.correct }} </p>
